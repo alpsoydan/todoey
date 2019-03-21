dea3bea4ad04976ee9664c645f1a04be1bace1f7
user150096 <user150096@eu208.local>
2019-03-21 12:49:34 +0000

    Convert to MVC design pattern.

---
+++ todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by user150096 on 3/21/19.
+//  Copyright © 2019 user150096. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
